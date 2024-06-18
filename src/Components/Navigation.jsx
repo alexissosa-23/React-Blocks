b400013e76631aad7604186e6c4a83c514c64851
alexissosa-23 <84998555+alexissosa-23@users.noreply.github.com>
2024-06-17 23:09:50 -0400

    Trazando nuestros bloques de React

---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Navigation = () => {
+    return <div className="navigation"></div>;
+}
+
+export default Navigation;
---
+++ src/Components/SubContents.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SubContents = () => {
+    return <div className="subcontent"></div>;
+}
+
+export default SubContents;
